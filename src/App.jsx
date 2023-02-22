3583b948759996c6c98c9ea87c1042501a2aee91
Ashleigh Carr <Ashleigh.Carr@twohouses.co.uk>
2023-02-22 11:16:14 +0000

    Pages Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ForgotPassword from "./pages/ForgotPassword";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import Register from "./pages/Register";
+
 function App() {
     return (
-        <div>
-            <h1>Hello</h1>
-        </div>
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/offers" element={<Offers />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route
+                        path="/forgot-password"
+                        element={<ForgotPassword />}
+                    />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </Router>
+        </>
     );
 }
 
