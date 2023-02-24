a14f6754b8b2cc785d0fe02dbce08051286b79c3
Arun Purewal <arunpurewal94@gmail.com>
2023-02-24 12:09:35 +0000

    Edit Profile linked with Firebase

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
-import { getAuth } from "firebase/auth";
+import { getAuth, updateProfile } from "firebase/auth";
 import { useNavigate } from "react-router";
+import Swal from "sweetalert2";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase";
 
 export default function Profile() {
     const auth = getAuth();
@@ -10,10 +13,43 @@ export default function Profile() {
         email: auth.currentUser.email,
     });
     const { name, email } = formData;
+    const [changeDetail, setChangeDetail] = useState(false);
 
     function onLogout() {
         auth.signOut();
         navigate("/");
+    }
+
+    function onChange(e) {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.id]: e.target.value,
+        }));
+    }
+
+    async function onSubmit() {
+        try {
+            if (auth.currentUser.displayName !== name) {
+                await updateProfile(auth.currentUser, {
+                    displayName: name,
+                });
+                const docRef = doc(db, "users", auth.currentUser.uid);
+                await updateDoc(docRef, {
+                    name,
+                });
+                Swal.fire({
+                    icon: "success",
+                    title: "Awesome 😇",
+                    text: "Profile Details Updated.",
+                });
+            }
+        } catch (error) {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Could not be updated 😢",
+            });
+        }
     }
 
     return (
@@ -26,12 +62,16 @@ export default function Profile() {
                     <form className="space-y-5">
                         {/* Name Input */}
                         <input
-                            className="w-full px-4 py-2 text-gray-700 bg-white border-indigo-300 focus:border-indigo-500 rounded transition ease-in-out"
+                            className={`w-full px-4 py-2 text-gray-700 bg-white border-indigo-300 focus:border-indigo-500 rounded transition ease-in-out ${
+                                changeDetail &&
+                                "bg-gray-200 focus:bg-gray-200 text-gray-600"
+                            }`}
                             type="text"
                             value={name}
                             id="name"
-                            disabled
+                            disabled={!changeDetail}
                             placeholder="Name.."
+                            onChange={onChange}
                         />
                         <input
                             className="w-full px-4 py-2 text-gray-700 bg-white border-indigo-300 focus:border-indigo-500 rounded transition ease-in-out"
@@ -43,9 +83,13 @@ export default function Profile() {
                         />
                         <div className="flex items-center justify-between whitespace-nowrap">
                             <button
+                                onClick={() => {
+                                    changeDetail && onSubmit();
+                                    setChangeDetail((prevState) => !prevState);
+                                }}
                                 className="mt-5 bg-indigo-500 text-white px-7 py-3 rounded text-sm font-medium shadow-lg hover:bg-indigo-400 transition duration-200 ease-in hover:shadow-xl active:bg-indigo-800"
                                 type="button">
-                                Edit
+                                {changeDetail ? "Apply Change" : "Edit"}
                             </button>
                             <button
                                 onClick={onLogout}
